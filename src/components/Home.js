605e8ba3fce9684b1c112726d9bd20f1d583490d
Jared <jaredneilhortaleza11@gmail.com>
2024-04-05 21:09:26 +0800

    removed nav cursor highlight

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,16 +28,6 @@ const Home = () => {
         { duration: 500, fill: "forwards" }
       );
     });
-
-    document.getElementById("nav").onmousemove = (e) => {
-      const { currentTarget: target } = e;
-      const rect = target.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
-
-      target.style.setProperty("--mouse-x", `${x}px`);
-      target.style.setProperty("--mouse-y", `${y}px`);
-    };
   }, []);
 
   return (
