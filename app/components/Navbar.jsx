734476d159d3854ccac875bba2a2695f3d89e173
Jared Neil A. Hortaleza <jaredneilhortaleza11@gmail.com>
2024-07-23 21:49:01 +0800

    added mobile view for navbar and changed root layout

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,10 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 import "../styles/Navbar.css";
 
 const navLinks = [
@@ -20,6 +23,8 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <nav className="glow fixed top-0 left-0 right-0 z-10">
       <div className="flex flex-wrap items-center justify-between mx-auto px-8 py-3">
@@ -32,18 +37,33 @@ const Navbar = () => {
             height={100}
           />
         </Link>
-        <div className="menu hidden md:block md:w-auto">
-          <ul className="flex p-4 md:p-0 sm:flex-row md:space-x-14 mr-5">
-            {navLinks.map((link, index) => {
-              return (
-                <li key={index}>
-                  <NavLink href={link.path} title={link.title} />
-                </li>
-              );
-            })}
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 text-slate-200 hover:text-white"
+            >
+              <Bars3Icon className="h-10 w-10" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 text-slate-200 hover:text-white">
+              <XMarkIcon className="h-10 w-10" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-10 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );
 };
