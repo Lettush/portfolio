d03560edf64e47b21c1089149b3167a4757c195f
Jared Neil A. Hortaleza <jaredneilhortaleza11@gmail.com>
2024-07-25 15:54:40 +0800

    added project link

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -44,7 +44,7 @@ const projectsData = [
 
 const ProjectsSection = () => {
   return (
-    <>
+    <div id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
@@ -62,7 +62,7 @@ const ProjectsSection = () => {
           ))}
         </ul>
       </div>
-    </>
+    </div>
   );
 };
 
